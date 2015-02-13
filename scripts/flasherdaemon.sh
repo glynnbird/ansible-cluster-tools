49afafc6e1474fde0ecd54f82332ef5db064bb97
Glynn Bird <glynn.bird@gmail.com>
2015-02-13 15:15:13 +0000

    more mods

--- scripts/flasherdaemon.sh
+++ scripts/flasherdaemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi/flasher
 touch /home/pi/couchdb/dev/logs/node1.log
-/usr/bin/tail -f /home/pi/couchdb/dev/logs/node1.log | sudo /usr/local/bin/node ./flasher.js  > /dev/null &
+/usr/bin/tail -f /home/pi/couchdb/dev/logs/node1.log | grep --line-buffered 'FLASH' | sudo /usr/local/bin/node ./flasher.js  > /dev/null &
 
 
